0204664ad1399a76f136d575d28a85a2070929e6
Mehdi <rhouzm@gmail.com>
2022-09-02 21:09:20 +0200

    fix : ecdsa import

--- tests/unit/test_chain_cosmos.py
+++ tests/unit/test_chain_cosmos.py
@@ -1,7 +1,7 @@
 from aleph_client.chains.cosmos import CSDKAccount, get_fallback_account
 from aleph_client.chains.common import delete_private_key_file
 
-DEFAULT_HRP = 'cosmos' 
+DEFAULT_HRP = "cosmos" 
 
 def test_get_fallback_account():
     delete_private_key_file()
@@ -10,5 +10,4 @@ def test_get_fallback_account():
     assert account.CHAIN == "CSDK"
     assert account.CURVE == "secp256k1"
     assert account.hrp == DEFAULT_HRP
-    
     
